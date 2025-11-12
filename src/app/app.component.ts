79762cd1f36ac2087520fe74952a029f9ccfacee
nhaca <nhacawalter@gmail.com>
2025-11-12 13:00:22 +0200

    feet: adiciona componente de card reutilizavel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Plan {
+  type: string,
+  price: number,
+  style: 'orange' | 'purple'
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  plans: Plan[] = [
+    {
+      type: "Simples",
+      price: 100,
+      style: 'orange'
+    },
+    {
+      type: "Completo",
+      price: 200,
+      style: 'purple'
+    },
+  ]
 
+  onCardButtonClicked(type: string) {
+    console.log('onCardButtonClicked', type)
+    // Chamada HTTP
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
